cd2cb03408d0c1fd7f33e6833a23f7c56af36657
stephenglunt <saglunt@gmail.com>
2016-04-14 20:51:41 -0400

    Added files via upload
    These are some exceptions that I thought we could use.  We'll figure out what sort of messages to pop up for handling them in a bit.

---
+++ WrongCredentialsException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package log;
+
+/**
+ *
+ * @author Stephen
+ */
+class WrongCredentialsException extends Exception {
+
+    public WrongCredentialsException(String username) {
+    }
+    
+}
