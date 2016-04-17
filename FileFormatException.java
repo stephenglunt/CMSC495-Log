c8596eb960ad58458f2e8f3b6bb08db58471176c
stephenglunt <saglunt@gmail.com>
2016-04-17 06:27:31 -0400

    Added files via upload

---
+++ FileFormatException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Log;
+
+/**
+ *
+ * @author Stephen
+ */
+class FileFormatException extends Exception {
+
+    public FileFormatException(String string) {
+    }
+    
+}
