f791cbff7dbb3603b716b4c47f8fc11bbc360dce
stephenglunt <saglunt@gmail.com>
2016-04-23 17:55:39 -0400

    Update PasswordChangeException.java
    Updated comments.

--- PasswordChangeException.java
+++ PasswordChangeException.java
@@ -1,18 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * CMSC 194
  */
 package Log;
 
 /**
- *
+ * Eexception thrown if there is an error in changing password.
  * @author Stephen
  */
 class PasswordChangeException extends Exception {
     public String title;
     public String message;
 
+    /**
+     * Constructor
+     * @param ttl
+     * @param msg 
+     */
     public PasswordChangeException(String ttl, String msg) {
         title = ttl;
         message = msg;
